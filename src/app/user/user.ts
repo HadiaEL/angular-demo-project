62d57d5619513f9dd50156f79960968ce707ed45
haelamri <hadia.el-amri@capgemini.com>
2026-05-07 13:40:22 +0100

    outputing list content

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,11 +15,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 
 export class UserComponent {
-  @Input({required: true}) user !: { 
-    id: string; 
-    name: string; 
-    avatar: string 
-  };
+  @Input({required: true}) user !: User;
 
   @Output() selected = new EventEmitter<string>();
 
