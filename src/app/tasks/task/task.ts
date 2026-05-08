1846a32c96a3a4e72db31683d5dc3f26c006534f
haelamri <hadia.el-amri@capgemini.com>
2026-05-08 12:04:44 +0100

    combining props to one property task

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -7,16 +7,18 @@ import { Component, Input, Output } from '@angular/core';
   styleUrls: ['./task.css'],
 })
 export class TaskComponent {
-  @Input({required: true}) title!: string;
-  @Input() summary?: string;
-  @Input() dueDate?: string;
+  @Input({required: true}) task!: {
+    title: string;
+    summary?: string;
+    dueDate?: string;
+  };
 
 
   get selectedTask()  {   
     return {
-      title: this.title,
-      summary: this.summary,
-      dueDate: this.dueDate
+      title: this.task.title,
+      summary: this.task.summary,
+      dueDate: this.task.dueDate
     }
   }
 }
